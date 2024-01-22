37d6d82544b4cdb97980e7f0aedf20d208137112
Herandy Santos <emilesanen@gmail.com>
2024-01-21 23:48:17 -0400

    Refactoring component to render a list of flight data.

--- app/components/SearchResults.tsx
+++ app/components/SearchResults.tsx
@@ -1,5 +1,28 @@
-import React from "react"
+import { FlightDataResponse } from "app/api/FlightTracker"
+import React, { FC } from "react"
+import { ListView } from "./ListView"
+import { ContentStyle } from "@shopify/flash-list"
+import { spacing } from "app/theme"
+import { FlightCard } from "./FlightCard"
 
-export const SearchResults = () => {
-  return <></>
+export interface SearchResultsProps {
+  flights: FlightDataResponse[]
+}
+
+export const SearchResults: FC<SearchResultsProps> = (props) => {
+  return (
+    <>
+      <ListView<FlightDataResponse>
+        contentContainerStyle={$listContentContainer}
+        data={props.flights.slice()}
+        renderItem={(d) => <FlightCard flight={d.item} />}
+      />
+    </>
+  )
+}
+
+const $listContentContainer: ContentStyle = {
+  paddingHorizontal: spacing.lg,
+  paddingTop: spacing.lg + spacing.xl,
+  paddingBottom: spacing.lg,
 }
